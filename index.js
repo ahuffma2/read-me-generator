a3d71778b9613ef0bea3258463049ec341167ab7
Austin Huffman <austinhuffman0@gmail.com>
2021-07-09 20:11:34 -0400

    Prepping for submission

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-// const generateMarkdown = require('generateMarkdown');
 
 // TODO: Create an array of questions for user input
 //type, message , name
